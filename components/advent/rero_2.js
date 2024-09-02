8ba4888bb5c83ab67b8a66557a97c5e1b6ccd6fe
franciscoj <frankmazta@gmail.com>
2024-09-01 20:40:14 -0400

    se documentó este código

--- components/advent/rero_2.js
+++ components/advent/rero_2.js
@@ -4,22 +4,22 @@ function manufacture(gifts, materials) {
     return []
   }
 
-// const gifts = ['tren', 'oso', 'pelota'];
-// const materials = 'tronesa';
+const gifts = ['tren', 'oso', 'pelota'];
+const materials = 'tronesa';
 
-// manufacture(gifts, materials) // ["tren", "oso"]
-// 'tren' SÍ porque sus letras están en 'tronesa'
-// 'oso' SÍ porque sus letras están en 'tronesa'
-// 'pelota' NO porque sus letras NO están en 'tronesa'
+manufacture(gifts, materials) // ["tren", "oso"]
+'tren' SÍ porque sus letras están en 'tronesa'
+'oso' SÍ porque sus letras están en 'tronesa'
+'pelota' NO porque sus letras NO están en 'tronesa'
 
-// const gifts = ['juego', 'puzzle']
-// const materials = 'jlepuz'
+const gifts = ['juego', 'puzzle']
+const materials = 'jlepuz'
 
-// manufacture(gifts, materials) // ["puzzle"]
+manufacture(gifts, materials) // ["puzzle"]
 
-// const gifts = ['libro', 'ps5']
-// const materials = 'psli'
+const gifts = ['libro', 'ps5']
+const materials = 'psli'
 
-// manufacture(gifts, materials) // []
+manufacture(gifts, materials) // []
 
 
