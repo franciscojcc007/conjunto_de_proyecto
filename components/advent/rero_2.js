82a8095ce7c654afa45ec4116bc61f0f85e80066
franciscoj <frankmazta@gmail.com>
2024-09-01 22:07:06 -0400

    ok

--- components/advent/rero_2.js
+++ components/advent/rero_2.js
@@ -8,17 +8,17 @@ const gifts = ['tren', 'oso', 'pelota'];
 const materials = 'tronesa';
 
 manufacture(gifts, materials) // ["tren", "oso"]
-'tren' SÍ porque sus letras están en 'tronesa'
-'oso' SÍ porque sus letras están en 'tronesa'
-'pelota' NO porque sus letras NO están en 'tronesa'
+// 'tren' SÍ porque sus letras están en 'tronesa'
+// 'oso' SÍ porque sus letras están en 'tronesa'
+// 'pelota' NO porque sus letras NO están en 'tronesa'
 
-const gifts = ['juego', 'puzzle']
-const materials = 'jlepuz'
+// const gifts = ['juego', 'puzzle']
+// const materials = 'jlepuz'
 
 manufacture(gifts, materials) // ["puzzle"]
 
-const gifts = ['libro', 'ps5']
-const materials = 'psli'
+// const gifts = ['libro', 'ps5']
+// const materials = 'psli'
 
 manufacture(gifts, materials) // []
 
