bcf3548b0c40dfff17cadb27cdbecdcec4b4b22a
franciscoj <frankmazta@gmail.com>
2024-09-14 00:04:19 -0400

    creación de una variable d para todos lod document

--- components/portalDoctorStrange/estrutura.js
+++ components/portalDoctorStrange/estrutura.js
@@ -1,6 +1,8 @@
-let portal = document.getElementById("portal");
+const d = document;
+
+let portal = d.getElementById("portal");
 for (let i = 0; i < 360; i++) {
-  let chispa = document.createElement("div");
+  let chispa = d.createElement("div");
   chispa.className = "chispa";
   chispa.style.rotate = i * 2 + "deg";
   let chispaTranslation = Math.random() * (120 - 100) + 100;
