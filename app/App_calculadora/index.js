13004a72d445df17a97633ee26c2282cf7c15d68
franciscoj <frankmazta@gmail.com>
2024-09-16 15:18:06 -0400

    creación de una variable d para todos lod document

--- app/App_calculadora/index.js
+++ app/App_calculadora/index.js
@@ -1,8 +1,9 @@
 let runningTotal = 0;
 let buffer  = "0";
 let previosOperator;
+const d = document;
 
-const screen = document.querySelector('.screen');
+const screen = d.querySelector('.screen');
 
 function buttonClick(value){
      if(isNaN(value)){
@@ -86,7 +87,7 @@ function handleNumber(numerString){
      }
 }
 function init(){
-     document. querySelector('.calc-buttons').addEventListener('click', function(event){
+     d.querySelector('.calc-buttons').addEventListener('click', function(event){
           buttonClick(event.target.innerText);
      })
 }
