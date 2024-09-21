9b18c3da65dfbb96f19a4263c7ed9c60067b52ea
franciscoj <frankmazta@gmail.com>
2024-09-20 20:48:16 -0400

    se agrego la lógica para que un boton al hacer click en el aumente el doble de su largo para se vea la la img mas pikachu

---
+++ components/background_repeat/index.js
@@ -0,0 +1,12 @@
+
+const container = document.getElementById("pikachu");
+const button = document.getElementById("duplicateBtn");
+
+button.addEventListener("click", () => {
+    const currentWidth = container.offsetWidth;
+    const currentHeight = container.offsetHeight;
+
+//  Duplicar el tamaño del contenedor
+//  container.style.width = currentWidth * 2 + "px";
+    container.style.height = currentHeight + 128 + "px";
+});
