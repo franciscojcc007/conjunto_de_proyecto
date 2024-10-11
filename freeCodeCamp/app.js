1a2cb5383c8286b49b119d975063af20240a372a
franciscoj <frankmazta@gmail.com>
2024-10-11 16:04:17 -0400

    uso de tagName, typeof y innerHtml

--- freeCodeCamp/app.js
+++ freeCodeCamp/app.js
@@ -1,4 +1,13 @@
 const d = document;
 
 const contenedor = d.getElementById("contenedor");
-console.log(contenedor);
+console.log(contenedor.innerHTML);
+
+const tilulo = d.getElementById("titulo");
+console.log(tilulo.tagName);
+
+const queso = d.getElementById("queso");
+console.log(typeof queso);
+
+const toppings = d.getElementsByClassName("topping");
+console.log(toppings);
