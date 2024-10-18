37614c3e400adb3d0ad81d69b262ff2b2afddd04
franciscoj <frankmazta@gmail.com>
2024-10-18 19:54:17 -0400

    terminada el proyecto

---
+++ freeCodeCamp/pagina_colores/app.js
@@ -0,0 +1,22 @@
+const d = document;
+
+const bnt = d.getElementById('btn')
+const text = d.getElementById('color')
+
+const colorAleatorio = ()=>{
+    let dígitos = '01235456789ABCDEF';
+    let colorGuardado = '#';
+    for (let i = 0; i < 6; i++) {
+      colorGuardado += dígitos[Math.floor(Math.random() * 16)];
+      // let indice =  Math.floor(Math.random()*16)
+      // colorGuardado += dígitos[indice];
+    }
+    return colorGuardado;
+}
+bnt.addEventListener('click',function (params) {
+  let colorString = colorAleatorio();
+  // text.style.color = colorString;
+  text.textContent = colorString;
+  d.body.style.backgroundColor = colorString;
+  
+});
