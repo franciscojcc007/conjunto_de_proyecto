c7e1821c12fd13b1d0f39cc10ce4fd3d08ab7cdd
franciscoj <frankmazta@gmail.com>
2024-10-18 17:04:59 -0400

    prueba

--- components/practica_del_curso/prueva_junior.js
+++ components/practica_del_curso/prueva_junior.js
@@ -2,14 +2,32 @@
 // [1,1,1,2,10]
 // function que reciba un arry como parámetro que devuelva en menor, mayor y las suma de estos.
 
-function maxMin(number) {
-  const resultado = number.reduce((acc, number) => {
-      if (acc.max < number) acc.max = number;
-      if (acc.min > number) acc.min = number;
-      return acc;
-    },{ min: Infinity, max: 0 });
-    console.log(resultado)
-    console.log(resultado.min + resultado.max);
-    return resultado.min + resultado.max;
+// function maxMin(number) {
+//   const resultado = number.reduce(
+//     (acc, number) => {
+//       if (acc.max < number) acc.max = number;
+//       if (acc.min > number) acc.min = number;
+//       return acc;
+//     },
+//     { min: Infinity, max: 0 }
+//   );
+//   console.log(resultado);
+//   console.log(resultado.min + resultado.max);
+//   return resultado.min + resultado.max;
+// }
+// maxMin([1, 2, 3, 4, 56, 7, 8, 10]);
+
+const a = 1
+
+function suma() {
+  const b = 2
+  return suma2();
 }
-maxMin([1, 2, 3, 4, 56, 7, 8, 10]);
+const b = 3
+const c = 1
+
+function suma2() {
+  const c = 3
+  return a + b + c;
+}
+console.log(suma())
