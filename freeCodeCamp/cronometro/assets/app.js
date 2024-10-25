dcf53b6d72070e2ec5643f8848f7c99d1641a3ac
franciscoj <frankmazta@gmail.com>
2024-10-25 12:30:16 -0400

    se agrego la lógica tras los botones y  agredo una contante paras window

--- freeCodeCamp/cronometro/assets/app.js
+++ freeCodeCamp/cronometro/assets/app.js
@@ -1,5 +1,7 @@
 const d = document;
-const btnInicio = d.getElementById("btn-inicio-pausa");
+const w = window;
+const cronometro = d.getElementById("cronometro");
+const btnInicioPausa = d.getElementById("btn-inicio-pausa");
 const btnReiniciar = d.getElementById("btn-reiniciar");
 
 let [horas, minutos, segundos] = [0, 0, 0];
@@ -19,7 +21,36 @@ function actualizar() {
   const segundosFormato = asignarFormato(segundos);
   const minutosFormato = asignarFormato(minutos);
   const horasFormato = asignarFormato(horas);
+
+  cronometro.innerText = `${horasFormato}:${minutosFormato}:${segundosFormato}`;
 }
+
 function asignarFormato(unidadTiempo) {
   return unidadTiempo < 10 ? "0" + unidadTiempo : unidadTiempo;
 }
+
+btnInicioPausa.addEventListener('click', function(){
+  if (estado === "pausado") {
+    intervaloDeTiempo = w.setInterval(actualizar, 1000);
+    btnInicioPausa.innerHTML = '<img src="./assets/img/icon-park--pause.png" alt="icono del boton de pausa"></img>';
+    btnInicioPausa.classList.remove('iniciar')
+    btnInicioPausa.classList.add('pausa')
+    estado = "en curso";
+  }else{
+    w.clearInterval(intervaloDeTiempo);
+    btnInicioPausa.innerHTML = '<img src="./assets/img/solar--play-bold.png" alt="icono del boton de play"></img>';
+    btnInicioPausa.classList.remove('pausa')
+    btnInicioPausa.classList.add('iniciar')
+    estado = "pausado";
+  }
+})
+
+btnReiniciar.addEventListener('click', function(){
+  w.clearInterval(intervaloDeTiempo);
+  [horas, minutos, segundos] = [0, 0, 0];
+  cronometro.innerText = '00:00:00';
+  btnInicioPausa.innerHTML = '<img src="./assets/img/solar--play-bold.png" alt="icono del boton de play"></img>';
+  btnInicioPausa.classList.remove('pausa')
+  btnInicioPausa.classList.add('iniciar')
+  estado = 'pausado'
+});
