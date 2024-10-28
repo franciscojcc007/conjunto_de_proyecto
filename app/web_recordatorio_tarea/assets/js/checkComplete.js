f2feb069b7a767b7d762c10941025bb44d7ee203
franciscoj <frankmazta@gmail.com>
2024-10-28 12:21:41 -0400

    corrección de error del elemento i no mostraba el icono

--- app/web_recordatorio_tarea/assets/js/checkComplete.js
+++ app/web_recordatorio_tarea/assets/js/checkComplete.js
@@ -1,21 +1,21 @@
-const i = document.createElement("i");
-
+// checkComplete.js
 const checkComplete = () => {
+  const i = document.createElement("i");
+  i.classList.add("far", "fa-check-square", "icon");
+  i.addEventListener("click", completeTask); 
+  return i;
+};
 
-    i.classList.add("far", "fa-check-square", "icon");
-    i.addEventListener('click', completeTask);
-    return i;
-  };
+const completeTask = (event) => {
+  const element = event.currentTarget;
+  element.classList.toggle("fas");
+  element.classList.toggle("completeIcon");
+  element.classList.toggle("far");
 
-  const completeTask = (event) => {
-    const element = event.target;
-    element.classList.toggle('fas');
-    element.classList.toggle('completeIcon');
-    element.classList.toggle('far');
-    completeThrough(event)
-}
-function completeThrough(e){
-  let task = e.target.nextElementSibling;
-  task.classList.toggle('through')
-}
+  const taskText = element.nextElementSibling;
+  if (taskText) {
+      taskText.classList.toggle("through");
+  }
+};
+
 export default checkComplete;
