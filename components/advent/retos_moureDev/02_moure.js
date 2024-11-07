74ab35a4e715e946b3b369b371b4e85a01d58b9f
franciscoj <frankmazta@gmail.com>
2024-11-07 09:49:10 -0400

    parcial mente completado

--- components/advent/retos_moureDev/02_moure.js
+++ components/advent/retos_moureDev/02_moure.js
@@ -9,21 +9,57 @@
  * - Debes hacer print por consola del resultado de todos los ejemplos.
  *   (y tener en cuenta que cada lenguaje puede poseer más o menos posibilidades)
  *
- * DIFICULTAD EXTRA (opcional):
+ */
+let a = 2;
+let b = 3;
+function function1() {}
+function1();
+
+function function2(a, b = null) {
+  return a + b;
+}
+console.log(function2(2));
+
+const function3 = () => {
+  console.log("aro function");
+};
+function3();
+const function4 = function () {};
+function4();
+
+(function () {
+  console.log("Esto se ejecuta de inmediato");
+})();
+
+const multipleArgumento = (...names) => {
+  for (argumento of names) {
+    console.log(argumento);
+  }
+};
+multipleArgumento("juan", "jose", "ana");
+
+function usoF() {
+  const f = () => console.log("soy yo yo");
+  f();
+}
+
+usoF();
+// Variable local y global
+var global = 20
+function func() {
+  var local = 10
+  console.log(local, global)
+}
+
+func()
+
+/*
+* DIFICULTAD EXTRA (opcional):
  * Crea una función que reciba dos parámetros de tipo cadena de texto y retorne un número.
  * - La función imprime todos los números del 1 al 100. Teniendo en cuenta que:
  *   - Si el número es múltiplo de 3, muestra la cadena de texto del primer parámetro.
  *   - Si el número es múltiplo de 5, muestra la cadena de texto del segundo parámetro.
  *   - Si el número es múltiplo de 3 y de 5, muestra las dos cadenas de texto concatenadas.
  *   - La función retorna el número de veces que se ha impreso el número en lugar de los textos.
- *
- * Presta especial atención a la sintaxis que debes utilizar en cada uno de los casos.
- * Cada lenguaje sigue una convenciones que debes de respetar para que el código se entienda.
  */
-let a = 2;
-let b = 3;
-function function1(){}
-console.log("Función sin parámetros ni retorno");
-function1();
-function function2(a,b = null){return a + b}
-console.log(function2(2,))
+
