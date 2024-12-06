06e6c06b92bab3271e3e5512216fafc387713b84
franciscoj <frankmazta@gmail.com>
2024-12-06 19:09:12 -0400

    se cambio de ubicación por confusión que crea a realizar los restos por que son de diferente material educativo

---
+++ components/retos_moureDev/03_moure.js
@@ -0,0 +1,23 @@
+/*
+ * EJERCICIO:
+ * 1 Muestra ejemplos de creación de todas las estructuras soportadas por defecto
+ *   en tu lenguaje.
+ * 2 Utiliza operaciones de inserción, borrado, actualización y ordenación.
+ */
+
+let arr = ["jose", "mateo", "victor"];
+
+let lista;
+
+/*
+ * DIFICULTAD EXTRA (opcional):
+ * Crea una agenda de contactos por terminal.
+ * 1 Debes implementar funcionalidades de búsqueda, inserción, actualización
+ *   y eliminación de contactos.
+ * 2 Cada contacto debe tener un nombre y un número de teléfono.
+ * 3 El programa solicita en primer lugar cuál es la operación que se quiere realizar,
+ *   y a continuación los datos necesarios para llevarla a cabo.
+ * 4 El programa no puede dejar introducir números de teléfono no numéricos y con más
+ *   de 11 dígitos (o el número de dígitos que quieras).
+ * 5 También se debe proponer una operación de finalización del programa.
+ */
