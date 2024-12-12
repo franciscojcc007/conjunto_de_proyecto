434e914360fecfb89fd3ab53dd4ffa17db61ef41
franciscoj <frankmazta@gmail.com>
2024-12-11 20:40:29 -0400

    se elimino los números mágicos

--- app/fahr_a_cels/app.js
+++ app/fahr_a_cels/app.js
@@ -1,7 +1,10 @@
 let fahr = parseFloat(prompt("ingrese sus datos en fahrenheit"))
 
+
 const conversion =(fahr)=>{
-    let cels = ( (fahr -32) * (5 / 9)) ;
-    return cels;
+  const ajustarEscala = 32
+  const equivalente = 5/9;
+  let cels = ( (fahr - ajustarEscala) * (equivalente)) ;
+  return cels;
 }
 console.log(`La temperatura en grados celsius: ${Math.round(conversion(fahr))}`)
