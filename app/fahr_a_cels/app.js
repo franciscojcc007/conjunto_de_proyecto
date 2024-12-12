884202b6d57f6c6e71269312c55eee1da8f61e3c
franciscoj <frankmazta@gmail.com>
2024-12-12 12:41:45 -0400

    se nombro las variables english, y se cambio let por const

--- app/fahr_a_cels/app.js
+++ app/fahr_a_cels/app.js
@@ -2,9 +2,9 @@ let fahr = parseFloat(prompt("ingrese sus datos en fahrenheit"))
 
 
 const conversion =(fahr)=>{
-  const ajustarEscala = 32
-  const equivalente = 5/9;
-  let cels = ( (fahr - ajustarEscala) * (equivalente)) ;
+  const scaleAdjustment = 32
+  const carryEquivalentToFahr = 5/9;
+  const cels = ( (fahr - scaleAdjustment) * (carryEquivalentToFahr)) ;
   return cels;
 }
 console.log(`La temperatura en grados celsius: ${Math.round(conversion(fahr))}`)
