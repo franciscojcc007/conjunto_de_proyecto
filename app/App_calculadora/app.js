0a9d32615d6c9517150b4e852e8ba474c667000d
franciscoj <frankmazta@gmail.com>
2024-12-18 14:37:08 -0400

    usado el JavaScript Standard Style se unificaron las comillas en el código y se eliminaron algunos puntos y comas innecesarios

--- app/App_calculadora/app.js
+++ app/App_calculadora/app.js
@@ -1,93 +1,93 @@
-let runningTotal = 0;
-let buffer = "0";
-let previosOperator;
-const d = document;
+let runningTotal = 0
+let buffer = '0'
+let previosOperator
+const d = document
 
-const screen = d.querySelector(".screen");
+const $screen = d.querySelector('.screen')
 
 function buttonClick(value) {
   if (isNaN(value)) {
-    handleSymbol(value);
+    handleSymbol(value)
   } else {
-    handleNumber(value);
+    handleNumber(value)
   }
-  screen.innerText = buffer;
+  $screen.innerText = buffer
 }
 function handleSymbol(symbol) {
   switch (symbol) {
-    case "AC":
-      buffer = "0";
-      runningTotal = 0;
-      break;
-    case "=":
+    case 'AC':
+      buffer = '0'
+      runningTotal = 0
+      break
+    case '=':
       if (previosOperator === null) {
-        return;
+        return
       }
       flushOperation(parseInt(buffer));
-      previosOperator = null;
-      buffer = runningTotal;
-      runningTotal = 0;
-      break;
-    case "←":
+      previosOperator = null
+      buffer = runningTotal
+      runningTotal = 0
+      break
+    case '←':
       if (buffer.length === 1) {
-        buffer = "0";
-        break;
+        buffer = '0'
+        break
       } else {
-        buffer = buffer.substring(0, buffer.length - 1);
-        break;
+        buffer = buffer.substring(0, buffer.length - 1)
+        break
       }
-    case "%":
-      buffer = (parseFloat(buffer) / 100).toString();
-      break;
-    case "+":
-    case "−":
-    case "×":
-    case "÷":
-      handleMath(symbol);
-      break;
-    case ".":
-      if (!buffer.includes(".")) {
-        buffer += ".";
+    case '%':
+      buffer = (parseFloat(buffer) / 100).toString()
+      break
+    case '+':
+    case '−':
+    case '×':
+    case '÷':
+      handleMath(symbol)
+      break
+    case '.':
+      if (!buffer.includes('.')) {
+        buffer += '.'
       }
   }
 }
 
 function handleMath(symbol) {
-  if (buffer === "0") {
-    return;
+  if (buffer === '0') {
+    return
   }
   const intbuffer = parseInt(buffer);
 
   if (runningTotal === 0) {
-    runningTotal = intbuffer;
+    runningTotal = intbuffer
   } else {
-    flushOperation(intbuffer);
+    flushOperation(intbuffer)
   }
-  previosOperator = symbol;
-  buffer = "0";
+  previosOperator = symbol
+  buffer = '0'
 }
 
 function flushOperation(intbuffer) {
-  if (previosOperator === "+") {
-    runningTotal += intbuffer;
-  } else if (previosOperator === "−") {
-    runningTotal -= intbuffer;
-  } else if (previosOperator === "×") {
-    runningTotal *= intbuffer;
-  } else if (previosOperator === "÷") {
-    runningTotal /= intbuffer;
+  if (previosOperator === '+') {
+    runningTotal += intbuffer
+  } else if (previosOperator === '−') {
+    runningTotal -= intbuffer
+  } else if (previosOperator === '×') {
+    runningTotal *= intbuffer
+  } else if (previosOperator === '÷') {
+    runningTotal /= intbuffer
   }
 }
 function handleNumber(numberString) {
-  if (buffer === "0") {
-    buffer = numberString;
+  if (buffer === '0') {
+    buffer = numberString
   } else {
-    buffer += numberString;
+    buffer += numberString
   }
 }
 function init() {
-  d.querySelector(".calc-buttons").addEventListener("click", function (event) {
-    buttonClick(event.target.innerText);
+  d.querySelector('.calc-buttons').addEventListener('click', function (event) {
+    buttonClick(event.target.innerText)
   });
 }
-init();
+init()
