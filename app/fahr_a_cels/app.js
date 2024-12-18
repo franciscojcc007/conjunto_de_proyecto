60950ae370272d8f488db43e318e4619d57ee37e
franciscoj <frankmazta@gmail.com>
2024-12-18 15:09:34 -0400

    usado el JavaScript Standard Style se unificaron las comillas en el código y se eliminaron algunos puntos y comas innecesarios

--- app/fahr_a_cels/app.js
+++ app/fahr_a_cels/app.js
@@ -1,10 +1,10 @@
-let fahr = parseFloat(prompt("ingrese sus datos en fahrenheit"))
+let fahr = parseFloat(prompt('ingrese sus datos en fahrenheit'))
 
 
 const conversion =(fahr)=>{
   const scaleAdjustment = 32
-  const carryEquivalentToFahr = 5/9;
-  const cels = ( (fahr - scaleAdjustment) * (carryEquivalentToFahr)) ;
-  return cels;
+  const carryEquivalentToFahr = 5/9
+  const cels = ( (fahr - scaleAdjustment) * (carryEquivalentToFahr)) 
+  return cels
 }
 console.log(`La temperatura en grados celsius: ${Math.round(conversion(fahr))}`)
