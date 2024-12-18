3a14a6498a3d20dc37128729002c48a58c81a173
franciscoj <frankmazta@gmail.com>
2024-12-18 15:10:13 -0400

    usado el JavaScript Standard Style se unificaron las comillas en el código y se eliminaron algunos puntos y comas innecesarios

--- app/web_recordatorio_tarea/assets/js/checkComplete.js
+++ app/web_recordatorio_tarea/assets/js/checkComplete.js
@@ -1,20 +1,20 @@
 export const checkComplete = () => {
-  const i = document.createElement("i");
-  i.classList.add("far", "fa-check-square", "icon");
-  i.addEventListener("click", completeTask); 
-  return i;
-};
+  const i = document.createElement('i')
+  i.classList.add('far', 'fa-check-square', 'icon')
+  i.addEventListener('click', completeTask)
+  return i
+}
 
 const completeTask = (event) => {
-  const element = event.currentTarget;
-  element.classList.toggle("fas");
-  element.classList.toggle("completeIcon");
-  element.classList.toggle("far");
+  const element = event.currentTarget
+  element.classList.toggle('fas')
+  element.classList.toggle('completeIcon')
+  element.classList.toggle('far')
 
-  const taskText = element.nextElementSibling;
+  const taskText = element.nextElementSibling
   if (taskText) {
-      taskText.classList.toggle("through");
+      taskText.classList.toggle('through')
   }
-};
+}
 
 
