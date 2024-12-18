b038d8e1392390fcbe414a19cd649d4a7288e58f
franciscoj <frankmazta@gmail.com>
2024-12-18 15:10:22 -0400

    usado el JavaScript Standard Style se unificaron las comillas en el código y se eliminaron algunos puntos y comas innecesarios

--- app/web_recordatorio_tarea/assets/js/deleteIcon.js
+++ app/web_recordatorio_tarea/assets/js/deleteIcon.js
@@ -1,11 +1,11 @@
 export const deleteIcon = () => {
-    const i = document.createElement('i');
-    i.classList.add('fas', 'fa-trash-alt','trashIcon', 'icon');
-    i.addEventListener('click', deleteTask);
-    return i;
-  };
+    const i = document.createElement('i')
+    i.classList.add('fas', 'fa-trash-alt','trashIcon', 'icon')
+    i.addEventListener('click', deleteTask)
+    return i
+  }
 const deleteTask = (event) => {
-    const parent = event.target.parentElement;
-    parent.remove(); 
-};
+    const parent = event.target.parentElement
+    parent.remove() 
+}
 
