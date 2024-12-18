224b7fe72bfa45429727388a215a3a90a5611616
franciscoj <frankmazta@gmail.com>
2024-12-18 15:09:51 -0400

    usado el JavaScript Standard Style se unificaron las comillas en el código y se eliminaron algunos puntos y comas innecesarios

--- components/retos_moureDev/03_moure.js
+++ components/retos_moureDev/03_moure.js
@@ -53,92 +53,102 @@
 // require("colors");
 
 const agenda = () => {
-  const dict = {};
-  const readline = require("readline");
+  const dict = {}
+  const readline = require('readline')
   const rl = readline.createInterface({
     input: process.stdin,
-    output: process.stdout,
+    output: process.stdout
   });
 
   const menu = () => {
-    console.log(`\n1. Buscar contacto`);
-    console.log("2. Insertar contacto");
-    console.log("3. Actualizar contacto");
-    console.log("4. Eliminar contacto");
-    console.log("5. Finalizar programa");
+    console.log('\n1. Buscar contacto')
+    console.log('2. Insertar contacto')
+    console.log('3. Actualizar contacto')
+    console.log('4. Eliminar contacto')
+    console.log('5. Finalizar programa')
 
-    rl.question("\n¿Qué operación deseas realizar?: ", (opcion) => {
+    const addContactIfValid = (name, phono) => {
+      if (/^\d{1,11}$/.test(phono)) {
+        dict[name] = phono
+        console.log(`Contacto agregado: ${name} - ${phono}`)
+      } else {
+        console.log(
+          `Error, la longitud o formato del teléfono ${phono} no es correcto`
+        )
+      }
+    }
+
+    rl.question('\n¿Qué operación deseas realizar?: ', (opcion) => {
       switch (parseInt(opcion)) {
         case 1:
-          rl.question("Introduce el nombre del contacto a buscar: ", (name) => {
+          rl.question('Introduce el nombre del contacto a buscar: ', (name) => {
             if (dict[name]) {
               console.log(`El contacto ${name} tiene el número ${dict[name]}`);
-            }else{
+            } else {
               console.log(`No hay un contacto con el nombre ${name}`);
             }
-            menu();
-          });
-          break;
+            menu()
+          })
+          break
 
         case 2:
-          rl.question("Introduce un nombre: ", (name) => {
-            rl.question("Introduce un número: ", (phono) => {
-              if (/^\d{1,11}$/.test(phono)) {
-                dict[name] = phono;
-                console.log(`Contacto agregado: ${name} - ${phono}`);
-              }else{
-                console.log(`Error, la longitud o formato del teléfono ${phono} no es correcto`);
-              }
-              menu();
-            });
-          });
-          break;
+          rl.question('Introduce un nombre: ', (name) => {
+            rl.question('Introduce un número: ', (phono) => {
+              addContactIfValid(name, phono)
+              menu()
+            })
+          })
+          break
 
         case 3:
-          rl.question("Introduce el nombre del contacto a actualizar: ",(name) => {
-              if (dict[name]) {rl.question("Introduce el nuevo número de teléfono: ",(phono) => {
-                    if (/^\d{1,11}$/.test(phono)) {
-                      dict[name] = phono;
-                      console.log(`Contacto actualizado: ${name} - ${phono}`);
-                    }else{
-                      console.log(
-                        `Error, la longitud o formato del teléfono ${phono} no es correcto`
-                      );
-                    }
-                    menu();
-                  });
+          rl.question(
+            'Introduce el nombre del contacto a actualizar: ',
+            (name) => {
+              if (dict[name]) {
+                rl.question(
+                  'Introduce el nuevo número de teléfono: ',
+                  (phono) => {
+                    addContactIfValid(name, phono)
+                    menu()
+                  }
+                )
               } else {
-                console.log(`No hay un contacto con el nombre ${name}`);
-                menu();
+                console.log(`No hay un contacto con el nombre ${name}`)
+                menu()
               }
-            });
-          break;
+            }
+          )
+          break
 
         case 4:
-          rl.question("Introduce el nombre del contacto a eliminar: ",(name) => {
+          rl.question(
+            'Introduce el nombre del contacto a eliminar: ',
+            (name) => {
               if (dict[name]) {
-                delete dict[name];
-                console.log(`Contacto eliminado: ${name}`);
-              }else{
-                console.log(`No hay un contacto con el nombre ${name}`);
+                delete dict[name]
+                console.log(`Contacto eliminado: ${name}`)
+              } else {
+                console.log(`No hay un contacto con el nombre ${name}`)
               }
-              menu();
-            });
-          break;
+              menu()
+            }
+          )
+          break
 
         case 5:
-          console.log("Saliendo de la agenda.");
-          rl.close();
-          break;
+          console.log("Saliendo de la agenda.")
+          rl.close()
+          break
 
         default:
-          console.log("Opción no válida. Por favor, selecciona entre 1 y 5.");
-          menu();
+          console.log('Opción no válida. Por favor, selecciona entre 1 y 5.')
+          menu()
       }
-    });
-  };
+    }
+  )
+  }
 
-  menu();
-};
+  menu()
+}
 
-agenda();
+agenda()
