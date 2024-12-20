b1d185824865cd1f405aeedfa5f87f8419f6db8e
franciscoj <frankmazta@gmail.com>
2024-12-20 09:32:14 -0400

    usado el JavaScript Standard Style se unificaron las comillas en el código y se eliminaron algunos puntos y comas innecesarios

--- app/web_recordatorio_tarea/assets/script.js
+++ app/web_recordatorio_tarea/assets/script.js
@@ -1,34 +1,33 @@
-//( () => {
-import {checkComplete} from "./js/checkComplete.js";
-import {deleteIcon} from "./js/deleteIcon.js";
+import {checkComplete} from "./js/checkComplete.js"
+import {deleteIcon} from "./js/deleteIcon.js"
 
-const d = document;
-const btn = d.querySelector("[data-form-btncreate]");
-const input = d.querySelector("[data-form-input]");
-const list = d.querySelector("[data-list]");
+const d = document
+const btn = d.querySelector("[data-form-btncreate]")
+const input = d.querySelector("[data-form-input]")
+const list = d.querySelector("[data-list]")
 
 const createtask = (evento) => {
-  evento.preventDefault();
+  evento.preventDefault()
   if (input.value) {
     const value = input.value;
-    const task = d.createElement("li");
+    const task = d.createElement("li")
     task.classList.add("card");
     input.value = "";
-    const taskContent = d.createElement("div");
+    const taskContent = d.createElement("div")
     taskContent.classList.add("card-body");
-    const titleTask = d.createElement("span");
-    titleTask.classList.add("task");
-    titleTask.innerText = value;
-    taskContent.appendChild(checkComplete());
-    taskContent.appendChild(titleTask);
-    task.appendChild(taskContent);
-    task.appendChild(deleteIcon());
+    const titleTask = d.createElement("span")
+    titleTask.classList.add("task")
+    titleTask.innerText = value
+    taskContent.appendChild(checkComplete())
+    taskContent.appendChild(titleTask)
+    task.appendChild(taskContent)
+    task.appendChild(deleteIcon())
     list.appendChild(task);
   } else {
-    alert("por favor agregue una tarea");
+    alert("por favor agregue una tarea")
   }
 };
 
-btn.addEventListener("click", createtask);
+btn.addEventListener("click", createtask)
 
-//})();
+
