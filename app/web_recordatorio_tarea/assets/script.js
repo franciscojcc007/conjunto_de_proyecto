8cb01417e83c4f4dbcdd6b99c19a891fae1c707f
franciscoj <frankmazta@gmail.com>
2024-12-20 09:40:34 -0400

    mejorar la legibilidad del código unificando el estilo de comillas y renombrando variables para mayor claridad

--- app/web_recordatorio_tarea/assets/script.js
+++ app/web_recordatorio_tarea/assets/script.js
@@ -2,32 +2,33 @@ import {checkComplete} from "./js/checkComplete.js"
 import {deleteIcon} from "./js/deleteIcon.js"
 
 const d = document
-const btn = d.querySelector("[data-form-btncreate]")
-const input = d.querySelector("[data-form-input]")
-const list = d.querySelector("[data-list]")
+const $btn = d.querySelector('[data-form-btncreate]')
+const $input = d.querySelector('[data-form-input]')
+const $list = d.querySelector('[data-list]')
 
 const createtask = (evento) => {
   evento.preventDefault()
-  if (input.value) {
-    const value = input.value;
-    const task = d.createElement("li")
-    task.classList.add("card");
-    input.value = "";
-    const taskContent = d.createElement("div")
-    taskContent.classList.add("card-body");
-    const titleTask = d.createElement("span")
-    titleTask.classList.add("task")
+  if ($input.value) {
+    const value = $input.value;
+    const task = d.createElement('li')
+    const titleTask = d.createElement('span')
+    const taskContent = d.createElement('div')
+
+    task.classList.add('card');
+    $input.value = '';
+    taskContent.classList.add('card-body');
+    titleTask.classList.add('task')
     titleTask.innerText = value
     taskContent.appendChild(checkComplete())
     taskContent.appendChild(titleTask)
     task.appendChild(taskContent)
     task.appendChild(deleteIcon())
-    list.appendChild(task);
+    $list.appendChild(task);
   } else {
-    alert("por favor agregue una tarea")
+    alert('por favor agregue una tarea')
   }
 };
 
-btn.addEventListener("click", createtask)
+$btn.addEventListener('click', createtask)
 
 
