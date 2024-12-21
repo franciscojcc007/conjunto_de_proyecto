69d96654b3d26aec4b42e2cce5f6300ee3bb849d
franciscoj <frankmazta@gmail.com>
2024-12-21 11:39:49 -0400

    mejorar la legibilidad del código unificando el estilo de comillas y renombrando variables para mayor claridad

--- app/web_recordatorio_tarea/assets/script.js
+++ app/web_recordatorio_tarea/assets/script.js
@@ -1,5 +1,5 @@
-import {checkComplete} from "./js/checkComplete.js"
-import {deleteIcon} from "./js/deleteIcon.js"
+import {checkComplete} from "@/js/checkComplete.js"
+import {deleteIcon} from "@/js/deleteIcon.js"
 
 const d = document
 const $btn = d.querySelector('[data-form-btncreate]')
