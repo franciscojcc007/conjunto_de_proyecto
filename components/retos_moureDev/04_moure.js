c90a39fce49cefc92284272513af229f19224c2f
franciscoj <frankmazta@gmail.com>
2024-12-24 11:14:39 -0400

    se cambiara a su repositorio propio

--- components/retos_moureDev/04_moure.js
+++ components/retos_moureDev/04_moure.js
@@ -16,25 +16,25 @@
  * verificación...✔️
  */
 
-let string1 = "Mariana"
-let string2 = " ster "
-let string3 = "Hola, soy"
+let string1 = 'Mariana'
+let string2 = ' ster '
+let string3 = 'Hola, soy'
 let arr = [string3, string1]
 
 console.log(string2.trim())
 console.log(string1.split(''))
 console.log(string1.slice(4))
-console.log(string1[3]);
-console.log(string1.length);
+console.log(string1[3])
+console.log(string1.length)
 console.log(`${string1} ${string2}`)
 console.log(string1.repeat(3))
-console.log(string1.toUpperCase());
-console.log(string1.toLowerCase());
-console.log(string1.replace("Mariana", "Mariana1"));
-console.log(string1.split("i"));
-console.log(arr.join(" "))
+console.log(string1.toUpperCase())
+console.log(string1.toLowerCase())
+console.log(string1.replace('Mariana', 'Mariana1'))
+console.log(string1.split('i'))
+console.log(arr.join(' '))
 console.log(`name: ${string1} lastName: ${string2}`)
-console.log(string1.includes("a"));
+console.log(string1.includes('a'))
 
 
 
@@ -46,3 +46,19 @@ console.log(string1.includes("a"));
  * 2 Anagramas
  * 3 Isogramas
  */
+
+let word1 = 'mano'
+let word2 = 'ana'
+
+function wordChecker(word1, word2) {
+    if (word1 === word1.split('').reverse().join('')) {
+        console.log('Es palíndromo')
+    } else if (word2 === word1.split('').sort().join('')) {
+        console.log('Es anagrama')
+    } else if (word1.length === new Set(word1).size) {
+        console.log('Es isograma')
+    } else {
+        console.log('No es nada')
+    }
+}
+
