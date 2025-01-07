46d0ef833cde50979c9c0ba098670048b07a5fd7
franciscoj <frankmazta@gmail.com>
2025-01-07 14:06:42 -0400

    mejorar la legibilidad del código en estrutura.js con ajustes de formato

--- components/galerias_web/assets/estrutura.js
+++ components/galerias_web/assets/estrutura.js
@@ -1,44 +1,44 @@
-document.addEventListener("DOMContentLoaded", function() {
-   const d = document;
-   const w = window;
-   const modal = d.getElementById("gallery-modal");
-   const modalImg = d.querySelector(".modal-img");
-   const closeButton = d.querySelector(".close-button");
-   let currentIndex = -1;
+document.addEventListener("DOMContentLoaded", function () {
+  const d = document;
+  const w = window;
+  const modal = d.getElementById("gallery-modal");
+  const modalImg = d.querySelector(".modal-img");
+  const closeButton = d.querySelector(".close-button");
+  let currentIndex = -1;
 
-   const images = Array.from(d.querySelectorAll(".gallery-item"));
+  const images = Array.from(d.querySelectorAll(".gallery-item"));
 
-   const updateModalImage = (index) => {
-         modalImg.src = images[index].src;
-         currentIndex = index;
-   };
+  const updateModalImage = (index) => {
+    modalImg.src = images[index].src;
+    currentIndex = index;
+  };
 
-   images.forEach((item, index) => {
-         item.addEventListener("click", function() {
-            modal.style.display = "block";
-            updateModalImage(index);
-         });
-   });
+  images.forEach((item, index) => {
+    item.addEventListener("click", function () {
+      modal.style.display = "block";
+      updateModalImage(index);
+    });
+  });
 
-   closeButton.addEventListener("click", function() {
-         modal.style.display = "none";
-   });
+  closeButton.addEventListener("click", function () {
+    modal.style.display = "none";
+  });
 
-   w.addEventListener("click", function(e) {
-         if (e.target === modal) {
-            modal.style.display = "none";
-         }
-   });
+  w.addEventListener("click", function (e) {
+    if (e.target === modal) {
+      modal.style.display = "none";
+    }
+  });
 
-   w.addEventListener("keydown", function(e) {
-         if (modal.style.display === "block") {
-            if (e.key === "ArrowRight") {
-               updateModalImage((currentIndex + 1) % images.length);
-            } else if (e.key === "ArrowLeft") {
-               updateModalImage((currentIndex - 1 + images.length) % images.length);
-            } else if (e.key === "Escape") {
-               modal.style.display = "none";
-            }
-         }
-   });
+  w.addEventListener("keydown", function (e) {
+    if (modal.style.display === "block") {
+      if (e.key === "ArrowRight") {
+        updateModalImage((currentIndex + 1) % images.length);
+      } else if (e.key === "ArrowLeft") {
+        updateModalImage((currentIndex - 1 + images.length) % images.length);
+      } else if (e.key === "Escape") {
+        modal.style.display = "none";
+      }
+    }
+  });
 });
