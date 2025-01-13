bfdc6a87e5aacaf926d7320d11104caf6bf56ccd
franciscoj <frankmazta@gmail.com>
2025-01-13 10:06:26 -0400

    refactor: reescribir la función findSafestPath para mejorar la legibilidad y eficiencia

--- components/halloween.dev/reto_3.js
+++ components/halloween.dev/reto_3.js
@@ -1,19 +1,46 @@
-function findSafestPath(dream) {
-    for(let i = 1; i < dream[0].length; i++) {
-        dream[0][i] += dream[0][i - 1];
+// function findSafestPath(dream) {
+//     for(let i = 1; i < dream[0].length; i++) {
+//         dream[0][i] += dream[0][i - 1];
 
-    }
-    for(let i = 1; i < dream.length; i++) {
-        dream[i][0] += dream[i - 1][0];
-    }
-    for(let i = 1; i < dream.length; i++) {
-        for(let j = 1; j < dream[i].length; j++) {
-            dream[i][j] += Math.min(dream[i - 1][j], dream[i][j - 1]);
-        }
-    }
-    return dream[dream.length - 1][dream[0].length - 1];
+//     }
+//     for(let i = 1; i < dream.length; i++) {
+//         dream[i][0] += dream[i - 1][0];
+//     }
+//     for(let i = 1; i < dream.length; i++) {
+//         for(let j = 1; j < dream[i].length; j++) {
+//             dream[i][j] += Math.min(dream[i - 1][j], dream[i][j - 1]);
+//         }
+//     }
+//     return dream[dream.length - 1][dream[0].length - 1];
     
+//   }
+
+
+function findSafestPath(dream){
+  const rowsNum = dream.length
+  const columnsNum = dream[0].length
+
+  const dangerLevels = []
+  dangerLevels[0] = dream[0][0]
+
+  for(let col = 1; col < columnsNum; col++){
+    dangerLevels[col] = dangerLevels[col - 1] + dream[0][col]
   }
+
+  console.log(dangerLevels)
+  for(let row = 1; row < rowsNum; row++){
+    dangerLevels[0] += dream[row][0]
+    for(let col = 1; col < columnsNum; col++){
+      dangerLevels[col] = Math.min(
+        dangerLevels[col - 1],
+        dangerLevels[col]
+      ) + dream[row][col]
+    }
+    console.log(dangerLevels)
+  }
+  return dangerLevels[columnsNum - 1]
+}
+
 
 const dream = [
     [1, 3, 1],
