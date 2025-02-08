adec16d10e6dd4b57ecf6f70e974f6b8035f6553
franciscoj <frankmazta@gmail.com>
2025-02-08 18:01:13 -0400

    Comentar ejemplos de bucles en as.js

--- as.js
+++ as.js
@@ -389,3 +389,16 @@
 // }
 // console.log(number)
 
+// for (let i = 1; i <= 20; i++) {
+//   if(i === 9){
+//     break
+//   }
+//   console.log(i);
+// }
+
+// for (let i = 1; i < 20; i++) {
+//   console.log(i);
+//   if(i % 3 === 0 && i % 5 === 0){
+//     break
+//   }
+// }
