38211fba3a332091419e18ce2404684a7dac257c
franciscoj <frankmazta@gmail.com>
2025-02-09 22:50:38 -0400

    Agregar ejemplos comentados de bucles en as.js

--- as.js
+++ as.js
@@ -402,3 +402,16 @@
 //     break
 //   }
 // }
+
+// for (let i = 1; i <= 20; i++) {
+//     if(i % 2 === 0){
+//         console.log(i);
+//     }
+// }
+
+for (let i = 1; i <= 20; i++) {
+  if (i % 4 === 0 ) {
+    continue;
+  }
+  console.log(i);
+}
