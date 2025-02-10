fd61c924dc7be6843d0e4ef682dffb6d0db26d7b
franciscoj <frankmazta@gmail.com>
2025-02-09 22:51:08 -0400

    ok

--- as.js
+++ as.js
@@ -409,9 +409,9 @@
 //     }
 // }
 
-for (let i = 1; i <= 20; i++) {
-  if (i % 4 === 0 ) {
-    continue;
-  }
-  console.log(i);
-}
+// for (let i = 1; i <= 20; i++) {
+//   if (i % 4 === 0 ) {
+//     continue;
+//   }
+//   console.log(i);
+// }
