b33ed37fcbe189f9ee257060ba42c6cd2fdd5c97
franciscoj <frankmazta@gmail.com>
2025-02-21 11:10:02 -0400

    Agregar función para contar regalos en una carta

---
+++ app/algorimia/advent/2021/r_2.js
@@ -0,0 +1,20 @@
+export default function listGifts(letter) {
+  
+  return {}
+}
+
+const carta = 'bici coche balón _playstation bici coche peluche'
+
+const regalos = listGifts(carta)
+
+console.log(regalos)
+
+
+/*
+{
+  bici: 2,
+  coche: 2,
+  balón: 1,
+  peluche: 1
+}
+*/
