9e94df9f0b1b5bd8fdbb4584c6d2df6ef19fec78
franciscoj <frankmazta@gmail.com>
2025-02-23 12:01:06 -0400

    Refactor la función startDrag para mejorar la legibilidad y eliminar comentarios innecesarios

--- app/Tinder_Finds_Love/src/app.js
+++ app/Tinder_Finds_Love/src/app.js
@@ -2,48 +2,34 @@ const d = document
 const DECISION_THRESHOLD = 75
 
 let isAnimating = false
-let pullDeltaX = 0 // distance from the card being dragged
+let pullDeltaX = 0 
 
 function startDrag(e) {
   if (isAnimating) return
 
-  // get the first article element
   const actualCard = e.target.closest('article')
   if (!actualCard) return
 
-  // get initial position of mouse or finger
   const startX = e.pageX ?? e.touches[0].pageX
 
-  // listen the mouse and touch movements
   d.addEventListener('mousemove', onMove)
   d.addEventListener('mouseup', onEnd)
-
   d.addEventListener('touchmove', onMove, { passive: true })
   d.addEventListener('touchend', onEnd, { passive: true })
 
   function onMove(e) {
-    // current position of mouse or finger
+
     const currentX = e.pageX ?? e.touches[0].pageX
 
-    // the distance between the initial and current position
     pullDeltaX = currentX - startX
 
-    // there is no distance traveled in X axis
     if (pullDeltaX === 0) return
 
-    // change the flag to indicate we are animating
     isAnimating = true
-
-    // calculate the rotation of the card using the distance
     const deg = pullDeltaX / 14
-
-    // apply the transformation to the card
     actualCard.style.transform = `translateX(${pullDeltaX}px) rotate(${deg}deg)`
-
-    // change the cursor to grabbing
     actualCard.style.cursor = 'grabbing'
 
-    // change opacity of the choice info
     const opacity = Math.abs(pullDeltaX) / 100
     const isRight = pullDeltaX > 0
 
@@ -55,20 +41,16 @@ function startDrag(e) {
   }
 
   function onEnd(e) {
-    // remove the event listeners
+
     d.removeEventListener('mousemove', onMove)
     d.removeEventListener('mouseup', onEnd)
-
     d.removeEventListener('touchmove', onMove)
     d.removeEventListener('touchend', onEnd)
 
-    // saber si el usuario tomo una decisión
     const decisionMade = Math.abs(pullDeltaX) >= DECISION_THRESHOLD
 
     if (decisionMade) {
       const goRight = pullDeltaX >= 0
-
-      // add class according to the decision
       actualCard.classList.add(goRight ? 'go-right' : 'go-left')
       actualCard.addEventListener('transitionend', () => {
         actualCard.remove()
@@ -76,22 +58,19 @@ function startDrag(e) {
     } else {
       actualCard.classList.add('reset')
       actualCard.classList.remove('go-right', 'go-left')
-
       actualCard.querySelectorAll('.choice').forEach(choice => {
         choice.style.opacity = 0
       })
     }
 
-    // reset the variables
+
     actualCard.addEventListener('transitionend', () => {
       actualCard.removeAttribute('style')
       actualCard.classList.remove('reset')
-
       pullDeltaX = 0
       isAnimating = false
     })
 
-    // reset the choice info opacity
     actualCard
       .querySelectorAll(".choice")
       .forEach((el) => (el.style.opacity = 0));
