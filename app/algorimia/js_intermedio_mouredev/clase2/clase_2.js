4bfb86ff1c6453b94c8ecb489c4a1f2b2a664adc
franciscoj <frankmazta@gmail.com>
2025-02-27 12:23:03 -0400

    Limpiar código comentado y reorganizar variables en clase_2.js

--- app/algorimia/js_intermedio_mouredev/clase2/clase_2.js
+++ app/algorimia/js_intermedio_mouredev/clase2/clase_2.js
@@ -1,18 +1,28 @@
-// let numbers = [1, 2, 3, 4, 5, 6]
+let sorted = [1, 1, 3, 3, 5, 5]
 
 // let filtrado = numbers.filter(el => el % 2 === 0)
 // console.log(filtrado)
 
 // flat
 
-let nestedArray = [1, [2, [3, [4,[5,[6]]]]]]
-let flatArray = nestedArray.flat(Infinity)
-console.log(flatArray)
+// let nestedArray = [1, [2, [3, [4,[5,[6]]]]]]
+// let flatArray = nestedArray.flat(Infinity)
+// console.log(flatArray)
 
 
 // flatMap
 
-let phrases = ["Hola mundo", "Adiós mundo"]
-let words = phrases.flatMap(phrase => phrase.split(' '))
-console.log(words)
+// let phrases = ["Hola mundo", "Adiós mundo"]
+// let words = phrases.flatMap(phrase => phrase.split(' '))
+// console.log(words)
+
+// let firstEvenIndex = sorted.findIndex(element => element % 2 === 0)
+// console.log(firstEvenIndex)
+
+// let numbers = new Set([1, 2, 2, 3, 7, 7])
+// let numbers2 = new Set([2,3,4,5])
+// let set = new Set([...numbers, ...numbers2])
+// let set2 = [...set]
+// console.log(set2)
+
 
