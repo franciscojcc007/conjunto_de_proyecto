bc5d6c2a87421ea78b15c70331809061aa6aa801
franciscoj <frankmazta@gmail.com>
2025-03-15 11:52:38 -0400

    Agregar archivo Mathematics.js con cálculos de geometría básica

---
+++ app/algorimia/Mathematics.js
@@ -0,0 +1,14 @@
+let ladoA = 5
+let ladoB = 4
+let base = 8
+let altura = 14
+let radio = 3
+
+console.log(`el perímetro del cuadrado es ${ladoA * 4}`)
+console.log(`el area del cuadrado es ${ladoA * ladoB}cm2`)
+console.log(`el perímetro del triangulo es ${ladoA + ladoB +ladoA}`)
+console.log(`el aria del triangulo es ${(base * altura) / 2}cm2`)
+console.log(`el diámetro del circulo es ${radio *2}`)
+console.log(`el circunferencia del circulo es ${(radio * 2) * Math.PI}`)
+console.log(`el area del circulo es ${(radio ** 2) * Math.PI}`)
+
