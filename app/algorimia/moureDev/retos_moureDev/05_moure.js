46de9b498cd0687063eb64966bfff9e5b97403b9
franciscoj <frankmazta@gmail.com>
2025-04-28 10:37:10 -0400

    Dificulta Extra completada

--- app/algorimia/moureDev/retos_moureDev/05_moure.js
+++ app/algorimia/moureDev/retos_moureDev/05_moure.js
@@ -6,17 +6,6 @@
  * 2 Muestra ejemplos de funciones con variables que se les pasan "por valor" y 
  *   "por referencia", y cómo se comportan en cada caso en el momento de ser modificadas.
  * (Entender estos conceptos es algo esencial en la gran mayoría de lenguajes)
- *
- * 
- * DIFICULTAD EXTRA (opcional):
- * Crea dos programas que reciban dos parámetros (cada uno) definidos como
- * variables anteriormente.
- * - Cada programa recibe, en un caso, dos parámetros por valor, y en otro caso, por referencia.
- *   Estos parámetros los intercambia entre ellos en su interior, los retorna, y su retorno
- *   se asigna a dos variables diferentes a las originales. A continuación, imprime
- *   el valor de las variables originales y las nuevas, comprobando que se ha invertido
- *   su valor en las segundas.
- *   Comprueba también que se ha conservado el valor original en las primeras.
  */
 
 // Tipos de datos por valor:
@@ -36,14 +25,51 @@
 
 
 // por valor
-let a = 5;
-let b = 'hola';
-let c= [1,1,1,];
+
 
 // por referencia
-let arr1 =[1, 2];
-let arr2 = arr1;
-arr2.push(3);
-console.log(arr2)
-console.log(arr1);
+// let arr1 =[1, 2];
+// let arr2 = arr1;
+// arr2.push(3);
+// console.log(arr2)
+// console.log(arr1);
 
+/*
+* DIFICULTAD EXTRA (opcional):
+* Crea dos programas que reciban dos parámetros (cada uno) definidos como
+* variables anteriormente.
+* - Cada programa recibe, en un caso, dos parámetros por valor, y en otro caso, por referencia.
+*   Estos parámetros los intercambia entre ellos en su interior, los retorna, y su retorno
+*   se asigna a dos variables diferentes a las originales. A continuación, imprime
+*   el valor de las variables originales y las nuevas, comprobando que se ha invertido
+*   su valor en las segundas.
+*   Comprueba también que se ha conservado el valor original en las primeras.
+*/
+
+const pasoPorValor = (c, d)=>{
+    let temp = c
+    c = d
+    d = temp
+    return [c, d]
+}
+
+let a = 1
+let b = 2
+
+let [c, d ]= pasoPorValor(a,b)
+
+console.log(`${a} ${b}, ${c} ${d}`)
+
+
+
+const pasoPorReferencia = (c, d)=>{
+    let temp = c
+    c = d
+    d = temp
+    return [c, d]
+}
+
+let arr1 = [1 ,2]
+let arr2 = [3, 4]
+let [arr3, arr4] = pasoPorReferencia(arr1, arr2)
+console.log(`${arr1} ${arr2}, ${arr3} ${arr4}`)
