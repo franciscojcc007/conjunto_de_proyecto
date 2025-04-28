89b6eef5e5e97e9b143079e4b292fe1bd21f12d3
franciscoj <frankmazta@gmail.com>
2025-04-27 20:16:18 -0400

    los ejemplos de asignación por valor y por referencia.

--- app/algorimia/moureDev/retos_moureDev/05_moure.js
+++ app/algorimia/moureDev/retos_moureDev/05_moure.js
@@ -1,11 +1,13 @@
 /*
  * EJERCICIO:
- * - Muestra ejemplos de asignación de variables "por valor" y "por referencia", según
+ * 1 Muestra ejemplos de asignación de variables "por valor" y "por referencia", según
  *   su tipo de dato.
- * - Muestra ejemplos de funciones con variables que se les pasan "por valor" y 
+ * 
+ * 2 Muestra ejemplos de funciones con variables que se les pasan "por valor" y 
  *   "por referencia", y cómo se comportan en cada caso en el momento de ser modificadas.
  * (Entender estos conceptos es algo esencial en la gran mayoría de lenguajes)
  *
+ * 
  * DIFICULTAD EXTRA (opcional):
  * Crea dos programas que reciban dos parámetros (cada uno) definidos como
  * variables anteriormente.
@@ -16,3 +18,32 @@
  *   su valor en las segundas.
  *   Comprueba también que se ha conservado el valor original en las primeras.
  */
+
+// Tipos de datos por valor:
+// String (Cadenas de texto)
+// Number (Números: enteros y flotantes)
+// Boolean (Booleanos: true o false)
+// undefined (Valor no definido)
+// null (Ausencia intencional de un valor)
+// Symbol (Identificadores únicos e inmutables)
+// BigInt (Números enteros de gran tamaño)
+
+// Tipos de datos por referencia:
+// Object (Objetos en general)
+// Array (Arreglos)
+// Function (Funciones)
+// Date, RegExp, Map, Set, entre otros (Todos derivados de Object)
+
+
+// por valor
+let a = 5;
+let b = 'hola';
+let c= [1,1,1,];
+
+// por referencia
+let arr1 =[1, 2];
+let arr2 = arr1;
+arr2.push(3);
+console.log(arr2)
+console.log(arr1);
+
