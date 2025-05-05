03374e0b49dbff6290b43f221432d568fd078d29
franciscoj <frankmazta@gmail.com>
2025-05-05 12:12:20 -0400

    Actualizar datos de usuario y mejorar la lógica de validación en index.js

--- app/operadores/Actividad2_operadores_comparacion/js/index.js
+++ app/operadores/Actividad2_operadores_comparacion/js/index.js
@@ -1,15 +1,15 @@
 // Escribe aquí tus datos correctos
-const NOMBRE_CORRECTO="jab";
-const ALIAS_CORRECTO="soyjab"
-const PIN_CORRECTO=12;
-const PASSWORD_CORRECTO="1234";
+const NOMBRE_CORRECTO = "frank";
+const ALIAS_CORRECTO = "soy frank"
+const PIN_CORRECTO = 12;
+const PASSWORD_CORRECTO = "1234";
 
 /* Estos son los datos del usuario
 (modifícalos para comprobar que todo funciona correctamente)*/
 
-const NOMBRE="soyjab";
-const PIN=0;
-const PASSWORD="1234";
+const NOMBRE = "soy frank";
+const PIN = 1;
+const PASSWORD = "1234";
 
 /*
 Si el usuario ha escrito el mismo valor  en NOMBRE, PIN y PASSWORD (pero no ha dejado ninguno vacío): es "Bien"
@@ -29,7 +29,24 @@ En el resto de casos: "Mal" (color "red")
   : ______
 */
 
+//versión mejorada 
+const nombreValido = NOMBRE === NOMBRE_CORRECTO || NOMBRE === ALIAS_CORRECTO;
+const credencialValida = PIN === PIN_CORRECTO || PASSWORD === PASSWORD_CORRECTO;
+const tripleCoincidencia = NOMBRE == PIN && NOMBRE == PASSWORD && NOMBRE != '';
 
-function dibujar(valor){
-  document.querySelector(".resultado").innerHTML=valor;
+(tripleCoincidencia || (nombreValido && credencialValida))// se puede usar if() si lo prefieres
+  ? dibujar(`<div class="bien">Bien</div>`)
+  : dibujar(`<div class="mal">Mal</div>`);
+
+
+// version de la clase 
+((NOMBRE == PIN && NOMBRE == PASSWORD && NOMBRE != '') ||
+  (NOMBRE === NOMBRE_CORRECTO || NOMBRE === ALIAS_CORRECTO) &&
+  (PIN === PIN_CORRECTO || PASSWORD === PASSWORD_CORRECTO))
+  ? dibujar(`<div class="bien">Bien</div>`)
+  : dibujar(`<div class="mal">Mal</div>`)
+
+
+function dibujar(valor) {
+  document.querySelector(".resultado").innerHTML = valor;
 }
