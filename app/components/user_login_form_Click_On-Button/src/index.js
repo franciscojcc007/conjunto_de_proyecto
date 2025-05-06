0a96e2eceace5328c53230856d096b235accd3cd
franciscoj <frankmazta@gmail.com>
2025-05-06 12:53:19 -0400

    Agregar lógica para mostrar y ocultar el formulario de inicio de sesión

--- app/components/user_login_form_Click_On-Button/src/index.js
+++ app/components/user_login_form_Click_On-Button/src/index.js
@@ -3,6 +3,8 @@ const validpassword = "1234";
 const d = document;
 
 const form = d.getElementById('user-login');
+const loginButton = d.querySelector('.login');
+const closeButton = d.querySelector('.close');
 
 form.addEventListener('submit', function(e) {
   e.preventDefault();
@@ -10,8 +12,17 @@ form.addEventListener('submit', function(e) {
   const username = d.getElementById('username').value;
   const password = d.getElementById('password').value;
 
-  const openLogin =  username === validusername && password === validpassword ?
-  window.location.href = "http://192.168.32.49:5500/components/user_login_form_Click_On-Button/pag_bienvenida.html" : alert("Usuario o contraseña incorrectos");
+  (username === validusername && password === validpassword )
+  ?window.location.href = "http://192.168.32.49:5500/app/components/user_login_form_Click_On-Button/pag_bienvenida.html" 
+  : alert("Usuario o contraseña incorrectos");
 
 });
   
+loginButton.addEventListener('click', function() {
+  form.style.display = 'block';
+});
+
+
+closeButton.addEventListener('click', function() {
+  form.style.display = 'none';
+});
