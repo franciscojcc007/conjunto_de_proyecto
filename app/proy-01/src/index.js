f119edd5356d18e80a78a6017e7b644e54a355c4
franciscoj <frankmazta@gmail.com>
2025-05-19 10:19:57 -0400

    Eliminado

--- app/proy-01/src/index.js
+++
@@ -1,8 +0,0 @@
-const d = document
-const bntElemento = d.querySelector('button');
-const spanElemento = d.querySelector('span');
-
-bntElemento.addEventListener('click', ()=>{
-    const yourName = prompt('¿Cuál es tu nombre?')
-    spanElemento.textContent = yourName
-})
