0f4daa52ac31efce3e250602c2c65d9cc5353ace
franciscoj <frankmazta@gmail.com>
2025-07-06 19:20:58 -0400

    Mejorar la legibilidad del código en 05_moure.js; ajustar espacios en blanco y formato en las funciones pasoPorValor y pasoPorReferencia

--- components/algorimia/moureDev/05_moure.js
+++ components/algorimia/moureDev/05_moure.js
@@ -36,8 +36,7 @@
 
 /*
 * DIFICULTAD EXTRA (opcional):
-* Crea dos programas que reciban dos parámetros (cada uno) definidos como
-* variables anteriormente.
+* Crea dos programas que reciban dos parámetros (cada uno) definidos como variables anteriormente.
 * - Cada programa recibe, en un caso, dos parámetros por valor, y en otro caso, por referencia.
 *   Estos parámetros los intercambia entre ellos en su interior, los retorna, y su retorno
 *   se asigna a dos variables diferentes a las originales. A continuación, imprime
@@ -46,30 +45,29 @@
 *   Comprueba también que se ha conservado el valor original en las primeras.
 */
 
-const pasoPorValor = (c, d)=>{
-    let temp = c
-    c = d
-    d = temp
-    return [c, d]
+const pasoPorValor = (c, d) => {
+  let temp = c
+  c = d
+  d = temp
+  return [c, d]
 }
 
 let a = 1
 let b = 2
 
-let [c, d ]= pasoPorValor(a,b)
+let [c, d] = pasoPorValor(a, b)
 
 console.log(`${a} ${b}, ${c} ${d}`)
 
-
-
-const pasoPorReferencia = (c, d)=>{
-    let temp = c
-    c = d
-    d = temp
-    return [c, d]
+const pasoPorReferencia = (c, d) => {
+  let temp = c
+  c = d
+  d = temp
+  return [c, d]
 }
 
-let arr1 = [1 ,2]
+let arr1 = [1, 2]
 let arr2 = [3, 4]
+
 let [arr3, arr4] = pasoPorReferencia(arr1, arr2)
 console.log(`${arr1} ${arr2}, ${arr3} ${arr4}`)
