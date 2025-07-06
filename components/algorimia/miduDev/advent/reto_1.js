70b451d2cf322029d5a9c22e42a2d86a829a9436
franciscoj <frankmazta@gmail.com>
2025-07-06 11:20:51 -0400

    Actualizar el arreglo giftIds y limpiar comentarios en reto_1.js

--- components/algorimia/miduDev/advent/reto_1.js
+++ components/algorimia/miduDev/advent/reto_1.js
@@ -9,17 +9,16 @@ function findFirstRepeated(gifts) {
   return -1;
 }
 
-const giftIds = [2, 1, 3, 5, 3, 2];
+const giftIds = [2, 1, 3, 5, 3, 2, 5,];
 const firstRepeatedId = findFirstRepeated(giftIds);
-console.log(firstRepeatedId); // 3
-// Aunque el 2 y el 3 se repiten
-// el 3 aparece primero por segunda vez
+console.log(firstRepeatedId); 
+
 
 const giftIds2 = [1, 2, 3, 4];
 const firstRepeatedId2 = findFirstRepeated(giftIds2);
-console.log(firstRepeatedId2); // -1
-// Es -1 ya que no se repite ningún número
+console.log(firstRepeatedId2); 
+
 
 const giftIds3 = [5, 1, 5, 1];
 const firstRepeatedId3 = findFirstRepeated(giftIds3);
-console.log(firstRepeatedId3); // 5
+console.log(firstRepeatedId3);
