4cab4b7d8d05ded3e67eb29ed71ba07436ca7f02
franciscoj <frankmazta@gmail.com>
2025-10-22 17:02:13 -0400

    Comentar la función limesToCut y su llamada para deshabilitar temporalmente su ejecución

--- algorimia/prueva_jn.js
+++ algorimia/prueva_jn.js
@@ -19,27 +19,31 @@
 // console.log(updateTask(['Tarea 1', 'Tarea 2', 'Tarea 3'], 1, 'Tarea Actualizada')); // [['Tarea 1', 'Tarea Actualizada', 'Tarea 3'], ['Tarea 1', 'Tarea 2', 'Tarea 3']]
 
 
-function limesToCut(wedgesNeeded, limes) {
-  let w = 0
-  let c = 0
-  for(let lime of limes){
-    if( w >= wedgesNeeded) break
-    
-    switch(lime){
-      case 'small' :
-        w += 6
-          break
-      case 'medium':
-        w += 8
-          break
-      case 'large':
-        w += 10
-          break
-    }
+// function limesToCut(wedgesNeeded, limes) {
+//   let w = 0
+//   let c = 0
+//   for(let lime of limes){
+//     if( w >= wedgesNeeded) break
 
-    c++
-  }
+//     switch(lime){
+//       case 'small' :
+//         w += 6
+//           break
+//       case 'medium':
+//         w += 8
+//           break
+//       case 'large':
+//         w += 10
+//           break
+//     }
 
-  return c
-}
-console.log(limesToCut(25, ['small', 'large', 'large', 'medium', 'small'])) // 4
+//     c++
+//   }
+
+//   return c
+// }
+// console.log(limesToCut(25, ['small', 'large', 'large', 'medium', 'small'])) // 4
+
+
+
+
