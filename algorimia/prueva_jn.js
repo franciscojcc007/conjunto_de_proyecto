cfb880bb5276ffa071c3a193ea2dbd36d9ffdd44
franciscoj <frankmazta@gmail.com>
2025-10-22 17:11:43 -0400

    Agregar comentarios y definir la función quantities para calcular la cantidad de fideos y salsa necesarios en la lasaña

--- algorimia/prueva_jn.js
+++ algorimia/prueva_jn.js
@@ -44,6 +44,27 @@
 // }
 // console.log(limesToCut(25, ['small', 'large', 'large', 'medium', 'small'])) // 4
 
+//Además de reservar el tiempo, también debes asegurarte de tener suficiente salsa y fideos para
+//  cocinar la lasaña de tu Sueños. Por cada capa de fideos en su lasaña, necesitará 50 gramos de 
+// fideos. Por cada capa de salsa en su lasaña, Necesitarás 0,2 litros de salsa.
+
+//Defina la función que toma una matriz de capas como parámetro. La función determinará entonces el
+//  cantidad de fideos y salsa necesarios para preparar su comida. El resultado debe devolverse como 
+// un objeto con claves y .quantitiesnoodlessauce
 
 
+// function quantities(capas){
+//     let noodles = 0
+//     let sauce = 0
+//     for (let i = 0; i < capas.length; i++) {
+//         if(capas[i]=== 'noodles') noodles += 50
+//         if(capas[i]=== 'sauce') sauce += 0.2
+//     }
+//     return {
+//         noodles,
+//         sauce
+//     }
+// }
 
+// console.log(quantities(['sauce', 'noodles', 'sauce', 'meat', 'mozzarella', 'noodles']));
+// // => { noodles: 100, sauce: 0.4 }
